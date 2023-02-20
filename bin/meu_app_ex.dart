1f05d505693faa40c6062254cb8998945b8b09f2
Bruno Mota <bmotadev@gmail.com>
2023-02-20 14:19:03 -0300

    :sparkles: feat: usando try catch e finally

--- bin/meu_app_ex.dart
+++ bin/meu_app_ex.dart
@@ -1,5 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:meu_app_ex/meu_app_ex.dart' as meu_app_ex;
 
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app_ex.calculate()}!');
+  print("Digite um número");
+  var line = stdin.readLineSync(encoding: utf8);
+  try {
+    double numero = double.parse(line ?? ""); // pode ser nulo
+    print(numero);
+  } catch (e) {
+    print("Numero invalido: $line");
+  } finally { // é executado independente se deu erro ou não
+    print("Executando finally");
+  }
 }
